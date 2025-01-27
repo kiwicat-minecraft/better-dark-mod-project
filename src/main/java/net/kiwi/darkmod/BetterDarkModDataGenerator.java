0d6c6358c5422f1dabe8b74589667dc7271b94ce
kiwicat-minecraft <emgekilian@outlook.de>
2025-01-27 19:39:21 +0100

    Initial commit

---
+++ src/main/java/net/kiwi/darkmod/BetterDarkModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kiwi.darkmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterDarkModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
