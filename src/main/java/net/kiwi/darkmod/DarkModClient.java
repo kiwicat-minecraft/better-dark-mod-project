ee5610ad31ad4a4d5a362fdf849b1b4c907e1981
kiwicat-minecraft <emgekilian@outlook.de>
2025-01-28 14:22:50 +0100

    first changes

---
+++ src/main/java/net/kiwi/darkmod/DarkModClient.java
@@ -0,0 +1,10 @@
+package net.kiwi.darkmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DarkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
